b446213d0ca60c00252bde5a3a04dc453fced926
Brian Morrison II <brian@brianmorrison.me>
2019-12-03 09:21:03 -0600

    day 1 is working

--- Day1/handler/__init__.py
+++ Day1/handler/__init__.py
@@ -7,7 +7,7 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    selectedIndex = random.randint(1,5)
+    selectedIndex = random.randint(0,3)
     options = [
         "Nun",
         "Gimmel",
@@ -16,20 +16,3 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     ]
 
     return func.HttpResponse(f"Have a day: {options[selectedIndex]}")
-
-    # name = req.params.get('name')
-    # if not name:
-    #     try:
-    #         req_body = req.get_json()
-    #     except ValueError:
-    #         pass
-    #     else:
-    #         name = req_body.get('name')
-
-    # if name:
-    #     return func.HttpResponse(f"Hello {name}!")
-    # else:
-    #     return func.HttpResponse(
-    #          "Please pass a name on the query string or in the request body",
-    #          status_code=400
-    #     )
